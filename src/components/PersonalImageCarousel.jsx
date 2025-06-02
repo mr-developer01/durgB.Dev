bcb701bd394773110c597cfbe5d684f9d52db415
Rahul <rahulpratik1212@gmail.com>
2025-06-03 00:50:37 +0530

    update-2.2

--- src/components/PersonalImageCarousel.jsx
+++ src/components/PersonalImageCarousel.jsx
@@ -3,44 +3,24 @@ import { useState } from "react";
 const PersonalImageCarousel = () => {
   const images = [
     {
-      src: "https://images.unsplash.com/photo-1517887433267-127c316d81a6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      caption: "Me at a conference",
+      src: "a.JPG",
+      caption: "01",
     },
     {
-      src: "https://images.unsplash.com/photo-1651684215020-f7a5b6610f23?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      caption: "Me hiking in the hills",
+      src: "b.JPG",
+      caption: "02",
     },
     {
-      src: "https://images.unsplash.com/photo-1556157382-97eda2d62296?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      caption: "Me at a family event",
+      src: "c.JPG",
+      caption: "03",
     },
     {
-      src: "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      caption: "Me at work",
+      src: "e.JPG",
+      caption: "04",
     },
     {
-      src: "https://images.unsplash.com/photo-1602471615287-d733c59b79c4?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      caption: "Me at a friend's wedding",
-    },
-    {
-      src: "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      caption: "Me during a vacation",
-    },
-    {
-      src: "https://plus.unsplash.com/premium_photo-1661374927471-24a90ebd5737?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      caption: "Me at a cultural fest",
-    },
-    {
-      src: "https://images.unsplash.com/photo-1691335053993-c9ba8cfd65e7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      caption: "Me with colleagues",
-    },
-    {
-      src: "https://images.unsplash.com/photo-1691335053879-02096d6ee2ca?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      caption: "Me at a charity event",
-    },
-    {
-      src: "https://plus.unsplash.com/premium_photo-1681822348509-8bba30584da1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      caption: "Me celebrating Diwali",
+      src: "f.JPG",
+      caption: "05",
     },
   ];
 
@@ -65,7 +45,7 @@ const PersonalImageCarousel = () => {
             <img
               src={images[currentIndex].src}
               alt={images[currentIndex].caption}
-              className="w-full h-[500px] object-cover object-center"
+              className="w-full h-[600px] object-cover object-center"
             />
             <div className="absolute bottom-0 w-full bg-black bg-opacity-50 text-white text-center py-2">
               <p>{images[currentIndex].caption}</p>
--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -2,9 +2,9 @@ const PersonalInfo = () => (
   <section className="bg-gray-100 py-12">
     <div className="container mx-auto px-4 text-center">
       <img
-        src="mypic.JPG"
+        src="g.JPG"
         alt="Profile"
-        className="w-56 mx-auto mb-6 border-4 border-amber-900 rounded-xl"
+        className="h-[600px] mx-auto mb-6 border-4 border-amber-900 rounded-xl"
       />
       <h2 className="text-rajul-3xl font-bold mb-2">Rahul</h2>
       <p className="text-lg text-gray-600 mb-4">
