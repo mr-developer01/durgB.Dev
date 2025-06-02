300693226063e0f8a2d68a13d77f5081eafaa1a9
Rahul <rahulpratik1212@gmail.com>
2025-06-02 23:00:21 +0530

    update-2.1

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -2,9 +2,9 @@ const PersonalInfo = () => (
   <section className="bg-gray-100 py-12">
     <div className="container mx-auto px-4 text-center">
       <img
-        src="https://images.unsplash.com/photo-1497316730643-415fac54a2af?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src="mypic.JPG"
         alt="Profile"
-        className="w-40 h-40 rounded-full mx-auto mb-6 object-center object-cover"
+        className="w-56 mx-auto mb-6 border-4 border-amber-900 rounded-xl"
       />
       <h2 className="text-rajul-3xl font-bold mb-2">Rahul</h2>
       <p className="text-lg text-gray-600 mb-4">
