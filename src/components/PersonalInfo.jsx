0b745e08e2693caa0a0f6868dafd82e63b76201a
Rahul <rahulpratik1212@gmail.com>
2026-02-09 15:37:28 +0530

    new pic

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -2,13 +2,13 @@ const PersonalInfo = () => (
   <section className="bg-gray-100 py-12">
     <div className="container mx-auto px-4 text-center">
       <img
-        src="rahul_main.jpeg"
+        src="r_m.jpeg"
         alt="Profile"
         className="h-[600px] mx-auto mb-6 border-4 border-amber-900 rounded-xl"
       />
       <h2 className="text-rajul-3xl font-bold mb-2">Rahul</h2>
       <p className="text-lg text-gray-600 mb-2">
-        25 years | Medium Brown | 5'6"
+        25 years | Wheatish | 5'6"
       </p>
       <p className="text-gray-700 max-w-2xl mx-auto">
         Ziradei Indra Awash Surwal, Siwan, Bihar - 841245
